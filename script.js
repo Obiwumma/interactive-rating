344202cb3fb3c86e35e5440f4c72b65b89f694a5
Obiwumma Pascal <pobiwumma@gmail.com>
2025-04-12 22:18:28 +0100

    Adjusted class active junk

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ const ratingThree = document.getElementById("#3")
 const ratingFour = document.getElementById("#4")
 const ratingFive = document.getElementById("#5")
 
+const mainPage = document.getElementById("main")
+const successPage = document.getElementById("success")
+
 const submitBtn = document.getElementById("#submit")
+
+submitBtn.addEventListener("click", () => {
+  mainPage.classList.add("hidden")
+  successPage.classList.remove("hidden")
+  successPage.classList.add("active")
+})
