628b0669a8570748cab289fde0a100276943188b
Obiwumma Pascal <pobiwumma@gmail.com>
2025-04-12 21:49:46 +0100

    Targeted buttons with JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const ratingOne = document.getElementById("#1")
+const ratingTwo = document.getElementById("#2")
+const ratingThree = document.getElementById("#3")
+const ratingFour = document.getElementById("#4")
+const ratingFive = document.getElementById("#5")
+
+const submitBtn = document.getElementById("#submit")
